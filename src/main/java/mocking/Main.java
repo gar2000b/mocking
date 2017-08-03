94f5b328951041d4fad0f425668302bd90758c9d
Gary Black <gar2000b@yahoo.com>
2017-08-03 23:37:41 +0100

    Adding main class#.

---
+++ src/main/java/mocking/Main.java
@@ -0,0 +1,8 @@
+package mocking;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Please check src/test/java for test classes testing out Mockito etc...");
+	}
+}
